cf2f98024277ffe19d70b1205e8016ddf02a3584
Vasiliy Troyan <trovas@inbox.ru>
2014-10-20 15:33:44 +0700

    before hstore

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,4 +1,7 @@
 class DocumentsController < InheritedResources::Base
+  def new
+    @document = Document.new(document_type_id: params[:product_type_id])
+  end
   private
   def document_params
     params.require(:document).permit(:name, :registration_date)
