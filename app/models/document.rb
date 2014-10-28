859357dd0906b93e9de695e7d10f14ed5dd5faa9
Vasiliy Troyan <trovas@inbox.ru>
2014-10-28 14:02:56 +0600

    Before push on github

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,15 @@
 class Document < ActiveRecord::Base
   belongs_to :document_type
   store_accessor :properties
+
+  validates :name, presence: true
+  validate :validate_properties
+
+  def validate_properties
+    document_type.fields.each do |field|
+      if field.required? && properties[field.name].blank?
+        errors.add field.name, "must not be blank"
+      end
+    end
+  end
 end
