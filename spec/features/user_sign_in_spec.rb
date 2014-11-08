8d0a1e75423d537bd6f7547bc3b67268c68ebab2
Vasiliy Troyan <trovas@inbox.ru>
2014-11-08 10:35:44 +0600

    user sign in

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -5,14 +5,35 @@ feature 'Non authorized user can sign in', %q(
   As a non authorized user
   I want to be able sign in
 ) do
+
+  given!(:user) { create :user }
+
   scenario 'Existing user sign in' do
-    user = User.create(email: 'test@test.com', password: '12345678', password_confirmation: '12345678')
     visit '/'
     click_on I18n.t 'authorization.sign_in'
     fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
+    fill_in I18n.t('activerecord.attributes.user.password'), with: user.password
     click_on I18n.t('action.log_in')
+
     expect(page).to have_content I18n.t('devise.sessions.signed_in')
   end
-  scenario 'Non existing user try to sign in'
+
+  scenario 'User enter wrong email' do
+    visit '/'
+    click_on I18n.t 'authorization.sign_in'
+    fill_in 'Email', with: 'wrong@test.com'
+    fill_in I18n.t('activerecord.attributes.user.password'), with: user.password
+    click_on I18n.t('action.log_in')
+
+    expect(page).to have_content I18n.t('devise.failure.user.not_found_in_database')
+  end
+  scenario 'User enter wrong password' do
+    visit '/'
+    click_on I18n.t 'authorization.sign_in'
+    fill_in 'Email', with: user.email
+    fill_in I18n.t('activerecord.attributes.user.password'), with: 'bad_pwd'
+    click_on I18n.t('action.log_in')
+
+    expect(page).to have_content I18n.t('devise.failure.user.not_found_in_database')
+  end
 end
