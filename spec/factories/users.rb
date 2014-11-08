3ad66eb16d2810d019ab92ff2fc87e461fab128b
Vasiliy Troyan <trovas@inbox.ru>
2014-11-08 09:52:34 +0600

    user signed in

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    
+    email 'user@test.com'
+    password '12345678'
+    password_confirmation '12345678'
   end
 
 end
--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -6,12 +6,13 @@ feature 'Non authorized user can sign in', %q(
   I want to be able sign in
 ) do
   scenario 'Existing user sign in' do
+    user = User.create(email: 'test@test.com', password: '12345678', password_confirmation: '12345678')
     visit '/'
-    click_on 'Sign in'
-    fill_in 'Email', with: 'User1'
-    fill_in 'Password', with: '12345678'
-    click_on 'Sign in'
-    expect(page).to have_content 'Successfull'
+    click_on I18n.t 'authorization.sign_in'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on I18n.t('action.log_in')
+    expect(page).to have_content I18n.t('devise.sessions.signed_in')
   end
   scenario 'Non existing user try to sign in'
 end
